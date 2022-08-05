e70d779e021296c69b31a26bf8e48031fe5d53df
huhu-miao <102189364+huhu-miao@users.noreply.github.com>
2022-08-05 11:42:06 -0600

    Create scoreboard.py

---
+++ 100days_of_code_Python/snake_game_v2/scoreboard.py
@@ -0,0 +1,34 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("high_score.txt") as file:
+            self.high_score = int(file.read())
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}  HighScore: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        with open("high_score.txt", mode="w") as file:
+            file.write(f"{self.high_score}")
+        self.score = 0
+        self.clear()
+        self.update_scoreboard()
